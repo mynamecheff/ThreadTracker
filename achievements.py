59f8cce4da06448cba67b652bcc8c90eb05c67d7
mynamecheff <albertdepalo@gmail.com>
2023-05-23 12:27:47 +0200

    added achievements and leaderboard

---
+++ achievements.py
@@ -0,0 +1,16 @@
+# Achievement thresholds
+ACHIEVEMENT_THRESHOLD = {
+    'Phishing': {
+        'name': 'Phishing Expert',
+        'threshold': 4
+    },
+    'QRadar': {
+        'name': 'QRadar Master',
+        'threshold': 4
+    }
+}
+
+ACHIEVEMENT_MESSAGE = {
+    'Phishing': 'Achievement Unlocked: Phishing Expert!',
+    'QRadar': 'Achievement Unlocked: QRadar Master!'
+}
--- app.py
+++ app.py
@@ -1,124 +1,131 @@
-from flask import Flask,render_template,request,redirect,url_for,flash,send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import sqlite3 as sql
+from achievements import ACHIEVEMENT_THRESHOLD, ACHIEVEMENT_MESSAGE
 
-
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
 
 @app.route("/crud")
 def crud():
-    con=sql.connect("leaderboard.db")
-    con.row_factory=sql.Row
-    cur=con.cursor()
+    con = sql.connect("leaderboard.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
     cur.execute("select * from stats_data")
-    data=cur.fetchall()
-    return render_template("crud.html",datas=data)
+    data = cur.fetchall()
+    return render_template("crud.html", datas=data)
 
-@app.route("/add_data",methods=['POST','GET'])
+
+@app.route("/add_data", methods=['POST', 'GET'])
 def add_data():
-    if request.method=='POST':
-        name=request.form['name']
-        create_date=request.form['create_date']
-        take_ownership_timestamp=request.form['take_ownership_timestamp']
-        closed_incident_timestamp=request.form['closed_incident_timestamp']
-        sd_severity=request.form['sd_severity']
-        type=request.form['type']
+    if request.method == 'POST':
+        # Fetch form data
+        name = request.form['name']
+        create_date = request.form['create_date']
+        take_ownership_timestamp = request.form['take_ownership_timestamp']
+        closed_incident_timestamp = request.form['closed_incident_timestamp']
+        sd_severity = request.form['sd_severity']
+        incident_type = request.form['type']
 
-        con=sql.connect("leaderboard.db")
-        cur=con.cursor()
-        cur.execute("insert into stats_data(name, create_date,take_ownership_timestamp,closed_incident_timestamp,sd_severity,type) values (?, ?,?,?,?,?)",(name, create_date,take_ownership_timestamp,closed_incident_timestamp,sd_severity,type))
+        con = sql.connect("leaderboard.db")
+        cur = con.cursor()
+        cur.execute(
+            "insert into stats_data(name, create_date, take_ownership_timestamp, closed_incident_timestamp, sd_severity, type) values (?, ?, ?, ?, ?, ?)",
+            (name, create_date, take_ownership_timestamp, closed_incident_timestamp, sd_severity, incident_type))
         con.commit()
-        flash('stats Added','success')
+        flash('Stats added', 'success')
+        check_achievements(name, incident_type)  # Check for achievements after adding data
         return redirect(url_for("crud"))
+
     return render_template("add_data.html")
 
-@app.route("/edit_data/<string:id>",methods=['POST','GET'])
-def edit_data(id):
-    if request.method=='POST':
-        name=request.form['name']
-        create_date=request.form['create_date']
-        take_ownership_timestamp=request.form['take_ownership_timestamp']
-        closed_incident_timestamp=request.form['closed_incident_timestamp']
-        sd_severity=request.form['sd_severity']
-        type=request.form['type']
 
-        con=sql.connect("leaderboard.db")
-        cur=con.cursor()
-        cur.execute("update stats_data set name=?,create_date=?,take_ownership_timestamp=?,closed_incident_timestamp=?,sd_severity=?,type=? where id=?",(name, create_date,take_ownership_timestamp,closed_incident_timestamp,sd_severity,type,id))
+@app.route("/edit_data/<string:id>", methods=['POST', 'GET'])
+def edit_data(id):
+    if request.method == 'POST':
+        # Fetch form data
+        name = request.form['name']
+        create_date = request.form['create_date']
+        take_ownership_timestamp = request.form['take_ownership_timestamp']
+        closed_incident_timestamp = request.form['closed_incident_timestamp']
+        sd_severity = request.form['sd_severity']
+        incident_type = request.form['type']
+
+        con = sql.connect("leaderboard.db")
+        cur = con.cursor()
+        cur.execute(
+            "update stats_data set name=?, create_date=?, take_ownership_timestamp=?, closed_incident_timestamp=?, sd_severity=?, type=? where id=?",
+            (name, create_date, take_ownership_timestamp, closed_incident_timestamp, sd_severity, incident_type, id))
         con.commit()
-        flash('data Updated','success')
+        flash('Data updated', 'success')
+        check_achievements(name, incident_type)  # Check for achievements after editing data
         return redirect(url_for("crud"))
-    con=sql.connect("leaderboard.db")
-    con.row_factory=sql.Row
-    cur=con.cursor()
-    cur.execute("select * from stats_data where id=?",(id,))
-    data=cur.fetchone()
-    return render_template("edit_data.html",datas=data)
-    
-@app.route("/delete_data/<string:id>",methods=['GET'])
+
+    con = sql.connect("leaderboard.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from stats_data where id=?", (id,))
+    data = cur.fetchone()
+    return render_template("edit_data.html", datas=data)
+
+
+@app.route("/delete_data/<string:id>", methods=['GET'])
 def delete_data(id):
-    con=sql.connect("leaderboard.db")
-    cur=con.cursor()
-    cur.execute("delete from stats_data where id=?",(id,))
+    con = sql.connect("leaderboard.db")
+    cur = con.cursor()
+    cur.execute("delete from stats_data where id=?", (id,))
     con.commit()
-    flash('data Deleted','warning')
+    flash('Data deleted', 'warning')
     return redirect(url_for("crud"))
 
 
+@app.route("/leaderboard")
+def leaderboard():
+    con = sql.connect("leaderboard.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute(
+        "SELECT name, COUNT(*) AS incidents, 'Total' AS type FROM stats_data GROUP BY name UNION ALL SELECT name, COUNT(*) AS incidents, type FROM stats_data WHERE type = 'QRadar' GROUP BY name, type UNION ALL SELECT name, COUNT(*) AS incidents, type FROM stats_data WHERE type = 'Phishing' GROUP BY name, type ORDER BY type ASC, incidents DESC")
+    data = cur.fetchall()
+    return render_template("leaderboard.html", datas=data, ACHIEVEMENT_THRESHOLD=ACHIEVEMENT_THRESHOLD,
+                           ACHIEVEMENT_MESSAGE=ACHIEVEMENT_MESSAGE)
+
+@app.route("/achievements")
+def achievements():
+    con = sql.connect("leaderboard.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    achievements_data = []
+
+    for incident_type, threshold_info in ACHIEVEMENT_THRESHOLD.items():
+        threshold = threshold_info['threshold']
+        cur.execute(
+            "SELECT name, type, COUNT(*) AS incidents FROM stats_data WHERE type = ? GROUP BY name, type HAVING incidents >= ?",
+            (incident_type, threshold))
+        rows = cur.fetchall()
+        for row in rows:
+            # Convert sqlite3.Row to dictionary
+            data = dict(row)
+            data['achievement_name'] = threshold_info['name']
+            achievements_data.append(data)
+
+    return render_template("achievements.html", datas=achievements_data, ACHIEVEMENT_MESSAGE=ACHIEVEMENT_MESSAGE, ACHIEVEMENT_THRESHOLD=ACHIEVEMENT_THRESHOLD)
 
 
 
-@app.route("/index")
-def index():
-    con=sql.connect("db_web.db")
-    con.row_factory=sql.Row
-    cur=con.cursor()
-    cur.execute("select * from users")
-    data=cur.fetchall()
-    return render_template("index.html",datas=data)
 
-@app.route("/add_user",methods=['POST','GET'])
-def add_user():
-    if request.method=='POST':
-        uname=request.form['uname']
-        contact=request.form['contact']
-        con=sql.connect("db_web.db")
-        cur=con.cursor()
-        cur.execute("insert into users(UNAME,CONTACT) values (?,?)",(uname,contact))
-        con.commit()
-        flash('User Added','success')
-        return redirect(url_for("index"))
-    return render_template("add_user.html")
+def check_achievements(name, incident_type):
+    con = sql.connect("leaderboard.db")
+    cur = con.cursor()
+    cur.execute("SELECT COUNT(*) FROM stats_data WHERE name = ? AND type = ?", (name, incident_type))
+    incidents_count = cur.fetchone()[0]
+    if incident_type in ACHIEVEMENT_THRESHOLD and incidents_count >= ACHIEVEMENT_THRESHOLD[incident_type]['threshold']:
+        achievement_message = ACHIEVEMENT_MESSAGE[incident_type]
+        flash(f'Achievement Unlocked: {achievement_message}', 'info')
 
-@app.route("/edit_user/<string:uid>",methods=['POST','GET'])
-def edit_user(uid):
-    if request.method=='POST':
-        uname=request.form['uname']
-        contact=request.form['contact']
-        con=sql.connect("db_web.db")
-        cur=con.cursor()
-        cur.execute("update users set UNAME=?,CONTACT=? where UID=?",(uname,contact,uid))
-        con.commit()
-        flash('User Updated','success')
-        return redirect(url_for("index"))
-    con=sql.connect("db_web.db")
-    con.row_factory=sql.Row
-    cur=con.cursor()
-    cur.execute("select * from users where UID=?",(uid,))
-    data=cur.fetchone()
-    return render_template("edit_user.html",datas=data)
-    
-@app.route("/delete_user/<string:uid>",methods=['GET'])
-def delete_user(uid):
-    con=sql.connect("db_web.db")
-    cur=con.cursor()
-    cur.execute("delete from users where UID=?",(uid,))
-    con.commit()
-    flash('User Deleted','warning')
-    return redirect(url_for("index"))
-    
-if __name__=='__main__':
-    app.secret_key='admin123'
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run()
