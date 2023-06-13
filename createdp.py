fda520d950c1f07719f85d5a07eec51be788c81b
mynamecheff <albertdepalo@gmail.com>
2023-06-13 19:31:15 +0200

    fixed database error

--- createdp.py
+++ createdp.py
@@ -7,32 +7,26 @@ con = sql.connect('leaderboard.db')
 cur = con.cursor()
 
 #Drop  table if already exsist.
-#cur.execute("DROP TABLE IF EXISTS stats_data")
+cur.execute("DROP TABLE IF EXISTS stats_data")
+cur.execute("DROP TABLE IF EXISTS users")
 
 
-#Create  table  in db_web database
-#sql ='''CREATE TABLE "users" (
-#	"UID"	INTEGER PRIMARY KEY AUTOINCREMENT,
-#	"UNAME"	TEXT,
-#	"CONTACT"	TEXT
-#)'''
 
-
-# sql = '''CREATE TABLE "stats_data" (
-  #  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-   # "name" TEXT NOT NULL,
-    #"create_date" TEXT NOT NULL,
-    #"take_ownership_timestamp" TEXT NOT NULL,
-    #"closed_incident_timestamp" TEXT,
-    #"sd_severity" TEXT NOT NULL,
-    #"type" TEXT NOT NULL
-#)'''
-
-sql = '''CREATE TABLE "users" (
+sql = '''CREATE TABLE "stats_data" (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-    )'''
+    "create_date" TEXT NOT NULL,
+    "take_ownership_timestamp" TEXT NOT NULL,
+    "closed_incident_timestamp" TEXT,
+    "sd_severity" TEXT NOT NULL,
+    "type" TEXT NOT NULL
+)'''
+
+#sql = '''CREATE TABLE "users" (
+ #   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  #  "name" TEXT NOT NULL,
+   # "password" TEXT NOT NULL
+    #)'''
 
 cur.execute(sql)
 
